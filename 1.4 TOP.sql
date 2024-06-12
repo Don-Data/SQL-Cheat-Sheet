2df72befa54ee427fa57c1da0af28f7cef11a7f8
Don Data <87910874+Don-Data@users.noreply.github.com>
2024-06-12 20:27:31 +0200

    Update 1.4 TOP.sql

--- 1.4 TOP.sql
+++ 1.4 TOP.sql
@@ -1,4 +1,25 @@
+-- Microsoft SQL Server and Access
 SELECT TOP(2)
+  [First Name]
+FROM Employee
+ORDER BY Salary ASC
+
+-- MySQL and PostgreSQL
+SELECT
+  [First Name]
+FROM Employee
+ORDER BY Salary ASC
+LIMIT 2 
+  
+-- Oracle and DB2
+SELECT
+  [First Name]
+FROM Employee
+ORDER BY Salary ASC
+FETCH FIRST 2 ROWS ONLY 
+  
+-- Informix
+SELECT FIRST 2
   [First Name]
 FROM Employee
 ORDER BY Salary ASC
